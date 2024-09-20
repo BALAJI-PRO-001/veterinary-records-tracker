46204ed835d04fe0384cac4bbb6e64a276467b7b
DEEPAK-IAS <deepaksurya2002dgl@gmail.com>
2024-09-20 13:44:11 +0530

    Add files via upload

---
+++ client/public/js/header.component.js
@@ -0,0 +1,24 @@
+const anchors = document.querySelectorAll("a");
+
+const toggleBtn = document.querySelector(".navbar-toggler");
+function addClass() {
+    const items = document.querySelector(".items");
+    items.classList.toggle("items1")
+    items.classList.toggle("show");
+} 
+toggleBtn.addEventListener("click",addClass);
+
+async function logOut(e) {
+    e.preventDefault();
+    const data = await fetch("/api/v1/admin/log-out",{
+        method: "GET"
+    });
+    if(data.status === 200) {
+        window.location.href = "/";
+    }
+}
+
+anchors[0].addEventListener("click",() => {
+    window.location.href = "/home";
+})
+anchors[3].addEventListener("click",logOut)
