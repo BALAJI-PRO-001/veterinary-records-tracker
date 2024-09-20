ccee4a66cdc0a32a73aa116d757e2c4a3deb7dc5
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-09-20 13:05:38 +0530

    Feat: insert and select

--- backend/src/db/sqlite3/index.ts
+++ backend/src/db/sqlite3/index.ts
@@ -1,60 +1,111 @@
 import sqlite3 from "sqlite3";
-import { SQLITE3_DATABASE_DIR_PATH } from "../../utils/constants";
 import fs from "fs/promises";
-import queries from "./queries";
 
 
 
-async function connect(): Promise<sqlite3.Database> {
-  try {
-    await fs.access(SQLITE3_DATABASE_DIR_PATH);
-  } catch(err) {
-    await fs.mkdir(SQLITE3_DATABASE_DIR_PATH);
+class Sqlite3Error extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+};
+
+
+class Sqlite3 {
+  db: sqlite3.Database | null;
+
+  constructor() {
+    this.db = null;
   }
 
-  return new Promise((resolve, reject) => {
-    const db = new sqlite3.Database(SQLITE3_DATABASE_DIR_PATH + "database.db", (err) => {
-      if (err) {
-        reject(new Error("SQLite3 database connection error: " + err.message));
+  async connectAndCreateDatabase(path: string, databaseName: string): Promise<sqlite3.Database | Sqlite3Error> {
+    try {
+      await fs.access(path);
+    } catch(err) {
+      await fs.mkdir(path);
+    }
+  
+    return new Promise((resolve, reject) => {
+      const db = new sqlite3.Database(path + databaseName + ".db", (err) => {
+        if (err) {
+          reject(new Sqlite3Error("SQLite3 database connection error: " + err.message));
+        } else {
+          this.db = db;
+          resolve(db);
+        }
+      });
+    });
+  }
+
+
+
+  async createTables(...queries: string[]): Promise<void | Sqlite3Error> {
+    return new Promise((resolve, reject) => {
+      if (!this.db) {
+        return reject(new Sqlite3Error("Database not connected."));
+      }
+      
+      const promises = queries.map((query) => {
+        return new Promise<void>((resolveQuery, rejectQuery) => {
+          this.db!.run(query, (err) => {
+            if (err) {
+              rejectQuery(`${err.message} while executing query: "${query}"`)
+            } else {
+              resolveQuery();
+            }
+          });
+        });
+      });
+
+      Promise.all(promises)
+        .then(() => resolve())
+        .catch((err) => reject(err));
+    });
+  }
+
+
+
+  async insert(sql: string, ...values: any[]): Promise<void | Sqlite3Error> {
+    return new Promise((resolve, reject) => {
+      if (!this.db) {
+        return reject(new Sqlite3Error("Database not connected."));
+      }
+
+      this.db.run(sql, values, (err) => {
+        if (err) {
+          reject(new Sqlite3Error(`${err.message} while executing query: "${sql}"`));
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+
+
+  async select(sql: string, selectAll: boolean = false, ...values: any[]) {
+    return new Promise((resolve, reject) => {
+      if (!this.db) {
+        return reject(new Sqlite3Error("Database not connected."));
+      }
+
+      function callback(err: Error, rows: unknown[]) {
+        if (err) {
+          reject(new Sqlite3Error(`${err.message} while executing query: "${sql}"`));
+        } else {
+          resolve(rows);
+        }
+      }
+      
+      if (selectAll) {
+        return values.length > 0 ? this.db.all(sql, values, callback) : this.db.all(sql, callback);
       } else {
-        resolve(db);
+        return values.length > 0 ? this.db.get(sql, values, callback) : this.db.get(sql, callback);
       }
     });
-  });
+  }
+
 }
 
 
 
-async function createTables(): Promise<void> {
-  const db = await connect();
-  return new Promise((resolve, reject) => {
-    db.serialize(() => {
-      db.run(queries.CREATE_USERS_TABLE_SQL, (err) => {
-        if (err) {
-          reject(new Error("Error creating user table: " + err.message));
-        }
-      });
-
-      db.run(queries.CREATE_COWS_TABLE_SQL, (err) => {
-        if (err) {
-          reject(new Error("Error creating cows table: " + err.message));
-        }
-      });
-
-      db.run(queries.CREATE_INJECTION_INFO_AND_AI_DATES_TABLE_SQL, (err) => {
-        if (err){
-          reject(new Error("Error creating injection info and ai dates table" + err.message))
-        }
-
-        resolve();
-      });
-    });
-  });
-}
-
-
-
-export default {
-  connect,
-  createTables
-};
+export default new Sqlite3;
