3eb5ee10d209e782994280946c54e7a1303da20c
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-09-20 23:06:25 +0530

    Bug: updateUserById()

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,18 +1,176 @@
 import sqlite3 from "../db/sqlite3"
 import queries from "../db/sqlite3/queries";
 
-interface User {
-  id?: number,
+
+
+interface NewUser {
   name: string,
   phoneNumber: number,
   address: string
+}
+
+
+
+interface User {
+  id: number,
+  name: string,
+  phoneNumber: number,
+  address: string, 
+  isCurrentUser: boolean,
+  createdAt: string
+}
+
+
+
+interface UserDataToUpdate {
+  id?: number,
+  name?: string,
+  phoneNumber?: number,
+  address?: string, 
+  isCurrentUser?: boolean,
   createdAt?: string
 }
 
 
 
-async function addNewUser(user: User): Promise<void> {
-  await sqlite3.insert(queries.INSERT_USER_RECORD_SQL, user.name, user.phoneNumber, user.address);
-  const newUser = sqlite3.select(queries.SELECT_LAST_USER_RECORD_SQL);
-  console.log(newUser);
+interface UserReccordStructureInDB {
+  id: number;
+  name: string;
+  phone_number: number;
+  address: string,
+  is_current_user: number;
+  date_and_time: string;
 }
+
+
+
+function validatePhoneNumber(phoneNumber: number): void {
+  if (String(phoneNumber).length !== 10 || isNaN(phoneNumber)) {
+    throw new Error("Phone number must be a valid number with exactly 10 digits.");
+  }
+}
+
+
+
+function validateId(id: number): void {
+  if (!id) {
+    throw new Error("Id is null or undefined.");
+  }
+}
+
+
+
+async function addNewUser(newUser: NewUser): Promise<User> {
+  validatePhoneNumber(newUser.phoneNumber);
+  await sqlite3.insert(queries.INSERT_USER_RECORD_SQL, newUser.name, newUser.phoneNumber, newUser.address);
+  const createdUser = await sqlite3.select(queries.SELECT_LAST_USER_RECORD_SQL, false) as UserReccordStructureInDB;
+
+  return {
+    id: createdUser.id,
+    name: createdUser.name,
+    phoneNumber: createdUser.phone_number,
+    address: createdUser.address,
+    isCurrentUser: createdUser.is_current_user === 1 ? true : false,
+    createdAt: createdUser.date_and_time
+  }
+}
+
+
+
+async function getUserByPhoneNumber(phoneNumber: number): Promise<User | null> {
+  validatePhoneNumber(phoneNumber);
+  const user = await sqlite3.select(queries.SELECT_USER_RECORD_BY_PHONE_NUMBER_SQL, false, phoneNumber) as UserReccordStructureInDB;
+  if (!user) {
+    return null;
+  }
+  return {
+    id: user.id,
+    name: user.name,
+    phoneNumber: user.phone_number,
+    address: user.address,
+    isCurrentUser: user.is_current_user === 1 ? true : false,
+    createdAt: user.date_and_time
+  };
+}
+
+
+
+async function getUserById(id: number): Promise<User | null> {
+  validateId(id);
+
+  const user = await sqlite3.select(queries.SELECT_USER_RECORD_BY_ID_SQL, false, id) as UserReccordStructureInDB;
+  if (!user) {
+    return null;
+  }
+  return {
+    id: user.id,
+    name: user.name,
+    phoneNumber: user.phone_number,
+    address: user.address,
+    isCurrentUser: user.is_current_user === 1 ? true : false,
+    createdAt: user.date_and_time
+  };
+}
+
+
+
+async function getAllUsers(): Promise<User[]> {
+  const usersRecords = await sqlite3.select(queries.SELECT_ALL_USERS_RECORDS_SQL, true) as UserReccordStructureInDB[];
+  const users = usersRecords.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      phoneNumber: user.phone_number,
+      address: user.address, 
+      isCurrentUser: user.is_current_user === 1 ? true : false,
+      createdAt: user.date_and_time
+    }
+  });
+  return users;
+}
+
+
+
+async function updateUserById(id: number, userDataToUpdate: UserDataToUpdate): Promise<void> {
+  validateId(id);
+
+  for (let [key, value] of Object.entries(userDataToUpdate)) {
+    key = key.includes("phoneNumber") ? "phone_number" : key;
+    await sqlite3.update(queries.UPDATE_USER_RECORD_BY_USER_ID.replace("<column_name>", key), value, id);
+  }
+
+  // const updatedUser = await sqlite3.select(queries.SELECT_USER_RECORD_BY_ID_SQL, false, id) as UserReccordStructureInDB;
+  // return {
+  //   id: updatedUser.id,
+  //   name: updatedUser.name,
+  //   phoneNumber: updatedUser.phone_number,
+  //   address: updatedUser.address, 
+  //   isCurrentUser: updatedUser.is_current_user === 1 ? true : false,
+  //   createdAt: updatedUser.date_and_time
+  // }
+}
+
+
+
+async function deleteUserById(id: number): Promise<void> {
+  validateId(id);
+  await sqlite3.delete(queries.DELETE_USER_RECORD_BY_ID_SQL, id);
+}
+
+
+
+async function deleteAllUsers(): Promise<void> {
+  await sqlite3.delete(queries.DELETE_ALL_USERS_RECORDS_SQL);
+}
+
+
+
+export default {
+  addNewUser,
+  getUserByPhoneNumber,
+  getUserById,
+  getAllUsers,
+  updateUserById,
+  deleteUserById,
+  deleteAllUsers
+};
