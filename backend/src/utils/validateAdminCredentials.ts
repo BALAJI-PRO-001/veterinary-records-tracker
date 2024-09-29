faae4a0842e161e3f6d8bd161060c40f3f3e3d5a
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-09-29 14:00:54 +0530

    Modified

--- backend/src/utils/validateAdminCredentials.ts
+++ backend/src/utils/validateAdminCredentials.ts
@@ -4,23 +4,15 @@ import errorHandler from "./errorHandler";
 export default function validateAdminCredentials(req: Request, res: Response, next: NextFunction): void {
   const { email, password } = req.body;
   if (email === undefined && password === undefined) {
-    return next(errorHandler(400, "Both email and password are required."));
+    return next(errorHandler(400, "Bad Request: Both email and password are required."));
   } 
 
-  if (email === undefined) {
-    return next(errorHandler(400, "Bad Request: Email is required."));
+  if (email === undefined || email === "" || email === null) {
+    return next(errorHandler(400, "Bad Request: Email is required and cannot be (empty, null, or undefined)."));
   }
 
-  if (password === undefined) {
-    return next(errorHandler(400, "Bad Request: Password is required."));
-  }
-
-  if (email === null) {
-    return next(errorHandler(400, "Bad Request: Email cannot be null."));
-  }
-
-  if (password === null) {
-    return next(errorHandler(400, "Bad Request: Password cannot be null."));
+  if (password === undefined || password === "" || password === null) {
+    return next(errorHandler(400, "Bad Request: Password is required and cannot be (empty, null, or undefined)."));
   }
 
   next();
