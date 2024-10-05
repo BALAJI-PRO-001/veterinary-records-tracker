e1abc04b6a9a751eb2c55a52f4487809f4f7a890
DEEPAK-IAS <deepaksurya2002dgl@gmail.com>
2024-10-05 14:22:18 +0530

    Add files via upload

---
+++ client/public/js/utils/userInterface.js
@@ -0,0 +1,90 @@
+
+import validator from "./validator.js"
+
+export function setMessage(element,message) {
+  if(!element) {
+    throw new Error("given element is null");
+  } 
+  element.innerText = message;
+}
+
+export function setType(element,oldType,newType) {
+  if(!element) {
+    throw new Error("given element is null");
+  }
+  if(element.type == oldType) {
+    element.type = newType;
+  }
+  else {
+    element.type = oldType;
+  }
+}
+
+export function setIcon(element,oldClass,newClass) {
+  if(!element) {
+    throw new Error("Element not found...");
+  }
+  element.classList.toggle(oldClass);
+  element.classList.toggle(newClass);
+}
+
+export function setBorder(element,className) {
+  if(!element) {
+    throw new Error("given element is null");
+  } 
+  element.classList.add(className);
+}
+
+export function removeBorder(element,className) {
+  if(!element) {
+    throw new Error("given element is null");
+  } 
+  element.classList.remove(className);
+}
+
+export function ValidateNameandUpdateUi(e) {
+  const name = e.target.value;
+  const {isValid,message} = validator.isValidName(name);
+  const errElement = e.target.parentElement.querySelector("#Err-element");
+  if(name == "") {
+    removeBorder(e.target,"is-invalid")
+    setMessage(errElement,"");
+  }
+  if(!isValid) {
+    setBorder(e.target,"is-invalid");
+    setMessage(errElement,message);
+  }
+}
+
+export function fieldIsValid(e) {
+  console.log("welcome");
+  const value = e.target.value;
+  const errElement = e.target.parentElement.querySelector("#Err-element");
+  if(value == "") {
+    setBorder(e.target,"is-invalid");
+    setMessage(errElement,"Enter this column..")
+    return false;
+  }
+  if(value != "") {
+    removeBorder(e.target,"is-invalid");
+    setMessage(errElement,"")
+    return true;
+  }
+
+}
+
+export function validatePhoneandUpdateUi(e) {
+  const phoneNo = e.target.value;
+  const {isValid,message} = validator.isValidPhoneNo(phoneNo);
+  const errElement = e.target.parentElement.querySelector("#Err-element");
+  if(phoneNo == "") {
+    removeBorder(e.target,"is-invalid");
+    setMessage(errElement,"");
+    return;
+  } 
+  if(!isValid) {
+    setBorder(e.target,"is-invalid");
+    setMessage(errElement,message);
+  } 
+}
+
