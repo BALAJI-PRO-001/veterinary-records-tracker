b93ecd84a3458b316493ca934f6049fc5b07152a
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-10-06 12:19:07 +0530

    Feat: Tests

--- __tests__/controllers/recordController/updateCowRecord.test.ts
+++ __tests__/controllers/recordController/updateCowRecord.test.ts
@@ -60,4 +60,93 @@ describe("Record API Tests [ Update Cow Record ]", () => {
     expect(res.body.success).not.toBeTruthy();
     expect(res.body.message).toBe("Cow record not found for the specified cow id: 100");
   });
+
+
+
+  test("Test 5 [ Task: Try to update cow id ] [ Response: Bad Request 400 ]", async () => {
+    const res = await request(app).patch("/api/v1/records/1/cows/1")
+      .send({id: 100})
+      .set("Cookie", adminAccessToken!);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.statusCode).toBe(400);
+    expect(res.body.success).not.toBeTruthy();
+    expect(res.body.message).toBe("Bad Request: Cannot update cow id.");
+  });
+
+
+
+  test("Test 6 [ Task: Invalid cow data ] [ Response: Bad Request 400 ]", async () => {
+    const res = await request(app).patch("/api/v1/records/1/cows/1")
+      .send({})
+      .set("Cookie", adminAccessToken!);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.statusCode).toBe(400);
+    expect(res.body.success).not.toBeTruthy();
+    expect(res.body.message).toBe("Bad Request: Update failed, no data provided for update.");
+  });
+
+
+
+  test("Test 7 [ Task: Invalid cow name ] [ Response: Bad Request 400 ]", async () => {
+    const nameDataToUpdate = ["", null];
+    for (let data of nameDataToUpdate) {
+      const res = await request(app).patch("/api/v1/records/1/cows/1")
+        .send({name: data})
+        .set("Cookie", adminAccessToken!);
+      expect(res.statusCode).toBe(400);
+      expect(res.body.statusCode).toBe(400);
+      expect(res.body.success).not.toBeTruthy();
+      expect(res.body.message).toBe(`Bad Request: Cow name cannot be (empty, null or undefined).`);
+    }
+  });
+
+
+
+  test("Test 8 [ Task: Invalid cow breed ] [ Response: Bad Request 400 ]", async () => {
+    const breedDataToUpdate = ["", null];
+    for (let data of breedDataToUpdate) {
+      const res = await request(app).patch("/api/v1/records/1/cows/1")
+        .send({breed: data})
+        .set("Cookie", adminAccessToken!);
+      expect(res.statusCode).toBe(400);
+      expect(res.body.statusCode).toBe(400);
+      expect(res.body.success).not.toBeTruthy();
+      expect(res.body.message).toBe(`Bad Request: Cow breed cannot be (empty, null or undefined).`);
+    }
+  });
+
+
+
+
+  test("Test 9 [ Task: Invalid cow bullName ] [ Response: Bad Request 400 ]", async () => {
+    const bullNameDataToTest = ["", null];
+    for (let data of bullNameDataToTest) {
+      const res = await request(app).patch("/api/v1/records/1/cows/1")
+        .send({bullName: data})
+        .set("Cookie", adminAccessToken!);
+      expect(res.statusCode).toBe(400);
+      expect(res.body.statusCode).toBe(400);
+      expect(res.body.success).not.toBeTruthy();
+      expect(res.body.message).toBe(`Bad Request: Cow bullName cannot be (empty, null or undefined).`);
+    }
+  });
+
+
+
+  test("Test 10 [ Task: Update valid cow data ] [ Response: Updated Record 200 ]", async () => {
+    const res = await request(app).patch("/api/v1/records/1/cows/1")
+      .send({
+        name: "new Cow",
+        breed: "new Breed",
+        bullName: "new BullName"
+      })
+      .set("Cookie", adminAccessToken!);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.statusCode).toBe(200);
+    expect(res.body.success).toBeTruthy();
+    expect(res.body.data.cow.name).toBe("new Cow");
+    expect(res.body.data.cow.breed).toBe("new Breed");
+    expect(res.body.data.cow.bullName).toBe("new BullName");
+  });
 });
