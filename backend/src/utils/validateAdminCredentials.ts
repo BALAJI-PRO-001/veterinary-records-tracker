fc272cc9191194743187b49bc5ffd59306929627
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-10-06 21:07:38 +0530

    Modified

--- backend/src/utils/validateAdminCredentials.ts
+++ backend/src/utils/validateAdminCredentials.ts
@@ -1,19 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 import errorHandler from "./errorHandler";
+import validateFieldDataType from "./validateFieldDataType";
+import validateFieldValues from "./validateFieldValues";
 
 export default function validateAdminCredentials(req: Request, res: Response, next: NextFunction): void {
-  const { email, password } = req.body;
-  if (email === undefined && password === undefined) {
-    return next(errorHandler(400, "Bad Request: Both email and password are required."));
-  } 
-
-  if (email === undefined || email === "" || email === null) {
-    return next(errorHandler(400, "Bad Request: Email is required and cannot be (empty, null, or undefined)."));
+  try {
+    const fields = [
+      {property: {name: "email", value: req.body.email}, dataType: "string"},
+      {property: {name: "password", value: req.body.password}, dataType: "string"}
+    ];
+    validateFieldDataType(fields);
+    validateFieldValues(req.body);
+    next();
+  } catch(err) {
+    const errMessage = err instanceof Error ? err.message : String(err);
+    next(errorHandler(400, "Bad Request: Admin " + errMessage));
   }
-
-  if (password === undefined || password === "" || password === null) {
-    return next(errorHandler(400, "Bad Request: Password is required and cannot be (empty, null, or undefined)."));
-  }
-
-  next();
 }
