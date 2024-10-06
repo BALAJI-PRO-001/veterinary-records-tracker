523c84bc602933acf8ca69f5c549f87331cddcbc
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-10-06 13:22:49 +0530

    Modified

--- backend/src/utils/validateUserRequiredData.ts
+++ backend/src/utils/validateUserRequiredData.ts
@@ -17,9 +17,14 @@ export default function validateUserRequiredData(user: NewUser): UserDataValidat
   }
 
   const requiredFields: (keyof NewUser)[] = ["name", "phoneNumber", "address"];
+  const dataType = {name: "string", phoneNumber: "number", address: "string"};
   for (let field of requiredFields) {
     if (user[field] === undefined || user[field] === "" || user[field] === null) {
       throw new UserDataValidationError(400, `Bad Request: User ${field} is required and cannot be (empty, null, or undefined).`);
+    }
+
+    if (typeof user[field] !== dataType[field]) {
+      throw new UserDataValidationError(400, `Bad Request: User '${field}' must be a ${dataType[field]}`);
     }
   }
 }
