c15633a170465fe1e537b03ccfc82df81596a0e7
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-10-06 20:57:22 +0530

    Modified

--- backend/src/utils/validateFieldDataType.ts
+++ backend/src/utils/validateFieldDataType.ts
@@ -2,8 +2,9 @@
 
 export default function validateFieldDataType(fields: Array<{property: {name: string, value: any}, dataType: string}>): Error | void {
   for (let field of fields) {
+    const filedType = typeof field.property.value;
     if (typeof field.property.value !== field.dataType) {
-      throw new Error(`${field.property.name} must be a valid '${field.dataType}', but received a value of type '${typeof field.property.value}'.`);
+      throw new Error(`${field.property.name} must be a valid '${field.dataType}', but received a value of type '${field.property.value === null ? "null" : filedType}'.`);
     }
   }
 }
