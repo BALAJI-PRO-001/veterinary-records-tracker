c46835631b364dc74125ce3f8b495b5b1b1f057c
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-10-07 22:31:45 +0530

    Modified

--- backend/src/utils/validateAdminCredentials.ts
+++ backend/src/utils/validateAdminCredentials.ts
@@ -1,16 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import errorHandler from "./errorHandler";
-import validateFieldDataType from "./validateFieldDataType";
-import validateFieldValues from "./validateFieldValues";
+import validateFieldsDataTypeAndValue from "./validateFieldsDataTypeAndValue";
 
 export default function validateAdminCredentials(req: Request, res: Response, next: NextFunction): void {
   try {
+    if (Object.entries(req.body).length === 0) {
+      return next(errorHandler(400, "Bad Request: Both admin email and password is required."));
+    }
+
+    if (!req.body.email) {
+      return next(errorHandler(400, "Bad Request: Admin email is required and cannot be (empty, null or undefined)."));
+    }
+
+    if (!req.body.password) {
+      return next(errorHandler(400, "Bad Request: Admin password is required and cannot be (empty, null or undefined)."))
+    }
+
     const fields = [
       {property: {name: "email", value: req.body.email}, dataType: "string"},
       {property: {name: "password", value: req.body.password}, dataType: "string"}
     ];
-    validateFieldDataType(fields);
-    validateFieldValues(req.body);
+    validateFieldsDataTypeAndValue(fields);
     next();
   } catch(err) {
     const errMessage = err instanceof Error ? err.message : String(err);
