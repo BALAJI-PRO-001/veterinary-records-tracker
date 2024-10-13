2a950e20cad2a0a275346d9c12048b71dca2b286
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-10-13 23:39:11 +0530

    Feat: db upload functionality

--- client/public/js/dashboard.js
+++ client/public/js/dashboard.js
@@ -1,105 +1,55 @@
 import { extractCowInformation } from "./utils/common.js";
 
-const recordCountElement = document.getElementById("record-count");
 const alertBox = document.getElementById("alert-box");
-const spinner = document.getElementById("spinner");
-const mainContainer = document.getElementById("main-container");
-const tableContainer = document.getElementById("table-container");
 
-async function getRecordsFromServer() {
+const dbContainer = document.getElementById("db-container");
+const dbInputElement = dbContainer.querySelector("input");
+const dbMessageElement = dbContainer.querySelector("#db-message-element");
+
+
+
+dbInputElement.addEventListener("change", async (e) => {
+  const file = e.target.files[0];
+  if (file && file.name.split(".").pop() !== "db") {
+    dbMessageElement.classList.replace("text-success", "text-danger");
+    dbMessageElement.textContent = "You can only upload database files ....";
+    return;
+  } 
+
   try {
-    const res = await fetch("/api/v1/records/all");
+    const formData = new FormData();
+    formData.append("database", file);
+    dbMessageElement.classList.replace("text-danger", "text-success");
+    dbMessageElement.innerText = "Uploading database ....";
+
+    const res = await fetch("/api/v1/super-user/update/db", {
+      method: "PATCH",
+      body: formData
+    });
     const data = await res.json();
-    spinner.classList.add("d-none");
-    mainContainer.classList.remove("d-none");
 
     if (data.statusCode === 200) {
-      return data.data.records;
+      dbMessageElement.innerText = data.message; 
+      dbMessageElement.innerText = "Checking ....";
+
+      setTimeout(() => {
+        fetch("/").then((res) => {
+          if (res.ok) {
+            dbMessageElement.classList.replace("text-danger", "text-success");
+            dbMessageElement.innerText = "Server restated successfully ...."
+
+            setTimeout(() => {
+              dbMessageElement.innerText = "";
+            }, 2500);
+          }
+        }) 
+        .catch((err) => {
+          dbMessageElement.classList.replace("text-success", "text-danger");
+          dbMessageElement.innerText = "Server restarting failed ....";
+        });
+      }, 500);
     }
-    alertBox.classList.toggle("d-none");
-    alertBox.innerText = "Error: " + data.message;
   } catch(err) {
-    alertBox.classList.toggle("d-none");
     alertBox.innerText = "Error: " + err.message;
   }
-}
-
-
-
-getRecordsFromServer().then((records) => {
-  recordCountElement.innerText = `Records: ${records.length}`;
-  tableContainer.appendChild(createDynamicRecordsTable(records));
 });
-
-
-
-function click() {
-  console.log("HI");
-}
-
-
-function createDynamicRecordsTable(records) {
-  const table = document.createElement("table");
-  table.setAttribute("id", "records-table");
-  table.className += "table align-middle table-hover text-nowrap";
-
-  const thead = document.createElement("thead");
-  thead.className += "bg-primary text-white position-sticky top-0";
-
-  const tr = `
-    <tr class="border border-dark">
-      <th>Id</th>
-      <th>Name</th>
-      <th>Phone Number</th>
-      <th>Address</th>
-      <th>D&T In DB</th>
-    </tr>
-  `;
-
-  thead.innerHTML = tr;
-  table.appendChild(thead);
-
-  let tbody = document.createElement("tbody");
-  for (let record of records) {
-    const { 
-      cowNames, cowBreeds, bullNames, injectionNames, 
-      injectionPrices, givenAmounts, pendingAmounts, dates 
-    } = extractCowInformation(record.cows);
-
-    const tr = document.createElement("tr");
-    tr.className += "border border-dark";
-    tr.onclick = click;
-    const tdElements = `
-        <td class="border border-dark d-inline-block border-top-0 text-truncate" style="width: 100px">${record.user.id}</td>
-        <td class="border border-dark d-inline-block border-top-0 text-truncate" style="width: 100px">${record.user.name}</td>
-        <td class="border border-dark d-inline-block border-top-0 text-truncate" style="width: 100px">${record.user.phoneNumber}</td>
-        <td class="border border-dark d-inline-block border-top-0 text-truncate" style="width: 100px">${record.user.address}</td>
-        <td class="border border-dark d-inline-block border-top-0 text-truncate" style="width: 100px">${record.recordCreatedAt}</td>
-    `;
-    tr.innerHTML = tdElements;
-    tbody.appendChild(tr);
-  }
-  table.appendChild(tbody);
-  return table;
-}
-
-
-
-/* <td class="border border-dark">${cowNames}</td>
-<td class="border border-dark">${cowBreeds}</td>
-<td class="border border-dark">${bullNames}</td>
-<td class="border border-dark">${injectionNames}</td>
-<td class="border border-dark">${injectionPrices}</td>
-<td class="border border-dark">${givenAmounts}</td>
-<td class="border border-dark">${pendingAmounts}</td>
-<td class="border border-dark">${dates}</td> */
-
-
-// <th>Cow Names</th>
-// <th>Cow Breeds</th>
-// <th>Bull Names</th>
-// <th>Injection Names</th>
-// <th>Injection Prices</th>
-// <th>Given Amount</th>
-// <th>Pending Amount</th>
-// <th>Ai Dates</th>
