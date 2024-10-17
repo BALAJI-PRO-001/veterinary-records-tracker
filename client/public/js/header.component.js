ab0e7704aaccabed86474507bb06c766ccd9b27f
DEEPAK-IAS <deepaksurya2002dgl@gmail.com>
2024-10-17 14:58:45 +0530

    feat: some style

--- client/public/js/header.component.js
+++ client/public/js/header.component.js
@@ -2,11 +2,9 @@
 const toggleBtn =  document.getElementById("toggler");
 const navigationMenu = document.getElementById("toggle-menu");
 
-
 console.log(toggleBtn);
 
-
-
 toggleBtn.addEventListener("click",() => {
-    navigationMenu.classList.toggle("d-none");
+    navigationMenu.classList.toggle("d-none");  
 })
+
