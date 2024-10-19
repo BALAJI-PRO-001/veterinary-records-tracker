bf8d21a2b92990b1be71b52bac9634e5eb1e836b
DEEPAK-IAS <deepaksurya2002dgl@gmail.com>
2024-10-19 09:10:27 -0700

    feat: some dairy milks

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,5 +1,8 @@
 import { setIcon,setType } from "./utils/userInteraction.js";
+import { validateEmailAndUpdateEmailInputUI,validatePasswordAndUpdatePasswordInputUI } from "./utils/userInteraction.js";
+import { addValidationListenersToInputElement } from "./utils/common.js";
 
+const loginForm = document.forms[0];
 const emailInput = document.getElementById("email-input");  
 const passwordInput = document.getElementById("password-input")
 const rememberMe = document.getElementById("remember-me");
@@ -7,7 +10,15 @@ const eyeIcon = document.getElementById("eye-icon");
 const loginBTN = document.getElementById("login-btn")
 
 
+loginForm.reset();
+
 eyeIcon.addEventListener("click",() => {
   setIcon(eyeIcon,"fa-eye","fa-eye-slash");
   setType(passwordInput,"password","text");
 })
+
+
+addValidationListenersToInputElement(emailInput,() => validateEmailAndUpdateEmailInputUI(emailInput));
+addValidationListenersToInputElement(passwordInput,() => validatePasswordAndUpdatePasswordInputUI(passwordInput));
+
+
