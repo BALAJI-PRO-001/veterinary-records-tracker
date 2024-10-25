fea5c473b91b56e9df3c916b912a98219ef8868f
DEEPAK-IAS <deepaksurya2002dgl@gmail.com>
2024-10-25 16:30:11 +0530

    feat: spinner class d-none

--- client/public/js/home.js
+++ client/public/js/home.js
@@ -1,28 +1,19 @@
+import { toggleElementVisibility } from "./utils/userInteraction";
 
 
 
 const contentContainer = document.getElementById("content-container");
+const spinner = document.getElementById("spinner");
 
-async function getRecordsFromServer() {
-  const res = await fetch("/api/v1/records/all",{
-    method: "GET"
-  });
-  const data = await res.json();
+async function fetchRecordAndUpdateUI() {
+  try {
+    toggleElementVisibility(spinner,false,"d-none");
+    
 
-  if(data.statusCode == 200) {
-    return data.data.records;
+
+
+
+  } catch (err) {
+    console.log(err);
   }
-  return null;
 }
-
-async function showData(userRecords) {
-
-  const datas = await userRecords;
-  contentContainer.innerText = "";
-  datas.forEach((data) => {
-    console.log(data);
-  })
-
-}
-
-showData(getRecordsFromServer());
