859c484217f4f94bf0c29891868b958fd94a4710
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-11-02 13:03:35 +0530

    Feat: Db upload functionality.

--- client/public/js/dashboard.js
+++ client/public/js/dashboard.js
@@ -1,68 +1,65 @@
+
 const alertBox = document.getElementById("alert-box");
-
-const dbContainer = document.getElementById("db-container");
-const dbInputElement = dbContainer.querySelector("input");
-const dbMessageElement = dbContainer.querySelector("#db-message-element");
-const serverStatusElement = document.getElementById("server-status-element");
-const hardResetBTN = document.getElementById("hard-reset-btn");
+const serverInfoContainer = document.getElementById("server-info-container");
 
 
-dbInputElement.addEventListener("change", async (e) => {
+const dbAndRecordsManagementContainer = document.getElementById("db-and-records-management-container");
+const dbFileInput = dbAndRecordsManagementContainer.querySelector("#db-file-input");
+const dbUpdateBTN = dbAndRecordsManagementContainer.querySelector("#db-update-btn");
+const dbAndRecordsDivMessageEl = dbAndRecordsManagementContainer.querySelector("#message-element");
+
+
+dbFileInput.addEventListener("change", async (e) => {
   const file = e.target.files[0];
   if (file && file.name.split(".").pop() !== "db") {
-    dbMessageElement.classList.replace("text-success", "text-danger");
-    dbMessageElement.textContent = "You can only upload database files ....";
-    return;
-  } 
+    dbAndRecordsDivMessageEl.classList.remove("text-success");
+    dbAndRecordsDivMessageEl.classList.add("text-danger");
+    return dbAndRecordsDivMessageEl.innerText = "You can only upload database file ....";
+  }
+});
 
+
+dbUpdateBTN.addEventListener("click", async () => {
   try {
+    const dbFile = dbFileInput.files[0];
+    if (!dbFile) {
+      dbAndRecordsDivMessageEl.classList.remove("text-success");
+      dbAndRecordsDivMessageEl.classList.add("text-danger");
+      return dbAndRecordsDivMessageEl.innerText = "Please select a database file.";
+    }
+
+    const spinner = dbAndRecordsManagementContainer.querySelector("#spinner");
+    spinner.classList.remove("d-none");
     const formData = new FormData();
-    formData.append("database", file);
-    dbMessageElement.classList.replace("text-danger", "text-success");
-    dbMessageElement.innerText = "Uploading database ....";
+    formData.append("database", dbFile);
+    spinner.classList.remove("d-none");
 
     const res = await fetch("/api/v1/super-user/update/db", {
       method: "PATCH",
       body: formData
     });
     const data = await res.json();
- 
+    spinner.classList.add("d-none");
+
+    if (data.statusCode === 401) {
+      dbAndRecordsDivMessageEl.classList.remove("text-success");
+      dbAndRecordsDivMessageEl.classList.add("text-danger");
+      return dbAndRecordsDivMessageEl.innerText = "Your session has expired. Please log out and log back in to continue.";
+    }
+
     if (data.statusCode === 200) {
-      dbMessageElement.innerText = data.message; 
-      dbMessageElement.innerText = "Connecting to server ....";
-
-      serverStatusElement.classList.replace("text-success", "text-danger");
-      serverStatusElement.innerText = "Deactivated.";
-
-      setTimeout(() => {
-        fetch("/").then((res) => {
-          if (res.ok) {
-            dbMessageElement.classList.replace("text-danger", "text-success");
-            dbMessageElement.innerText = "Server restated successfully ....";
-
-            serverStatusElement.classList.replace("text-danger", "text-success");
-            serverStatusElement.innerText = "Alive.";
-
-            setTimeout(() => {
-              dbMessageElement.innerText = "";
-            }, 2500);
-          }
-        }) 
-        .catch((err) => {
-          dbMessageElement.classList.replace("text-success", "text-danger");
-          dbMessageElement.innerText = "Server restarting failed ....";
-          serverStatusElement.classList.replace("text-success", "text-danger");
-          serverStatusElement.innerText = "Deactivated.";
-        });
-      }, 500);
+      dbAndRecordsDivMessageEl.classList.remove("text-danger");
+      dbAndRecordsDivMessageEl.classList.add("text-success");
+      dbAndRecordsDivMessageEl.innerText = "Database file uploaded successfully.";
+      return setTimeout(() => {
+        dbAndRecordsDivMessageEl.innerText = "";
+      }, 1000);
     }
   } catch(err) {
+    alertBox.classList.remove("d-none");
     alertBox.innerText = "Error: " + err.message;
   }
+
 });
 
 
-
-hardResetBTN.addEventListener("click", async () => {
-  const res = await fetch("/api/v1/super-user/server-actions", {method: "POST"});
-});
