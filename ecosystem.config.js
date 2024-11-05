9e7a90e7a1e02618c25d53192eeb6962b988095f
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-11-05 09:22:49 +0530

    Feat: Env Configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,6 +16,10 @@ module.exports = {
       restart_delay: 1000,              // Delay between restarts (1 second)
       min_uptime: "60s",                // Minimum uptime before considering the app stable
       instance_var: "INSTANCE_ID",       // Unique identifier for each instance in a cluster
+      env: {
+        NODE_ENV: 'production',
+        PORT: process.env.PORT || 3000, // Ensure the app listens on the Render-provided PORT
+      },
     }
   ]
 };
